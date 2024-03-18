1b92d537290615022a17271d19517d0bcefa4c50
To Yui Kai <yuikai@nexmind.ai>
2024-03-18 16:38:56 +0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#app',
+  data: {
+    profileImg: './assets/images/avatar-jessica.jpeg',
+    name: 'Jessica Randall',
+    location: 'London, United Kingdom',
+    about: '"Front-end developer and avid reader."',
+    
+    platforms: [
+      'GitHub',
+      'Frontend Mentor',
+      'LinkedIn',
+      'Twitter',
+      'Instagram',
+    ]
+  },
+});
